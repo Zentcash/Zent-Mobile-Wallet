e3737f1011b45ce75ad9dbfbce991ff533613f2a
Carlos Sancho <csancho@gmail.com>
2019-09-02 16:33:29 +0200

    Update

--- src/Config.js
+++ src/Config.js
@@ -205,7 +205,7 @@ const Config = {
     /**
      * Displayed in the settings screen
      */
-    appVersion: 'v1.0.1',
+    appVersion: 'v1.0.0',
 
     /**
      * Base URL for us to chuck a hash on the end, and find a transaction
